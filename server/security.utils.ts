4a4ae667c4a6ec4e6dea3e6f07c4a03003508fbc
beataj <beata@emc2wave.com>
2022-07-29 22:44:51 -0700

    create security.utils

--- server/security.utils.ts
+++ server/security.utils.ts
@@ -4,4 +4,14 @@ import crypto = require('crypto');
 import  util  = require('util');
 
 
-util.promisify(crypto.randomBytes);
+const randomBytes = util.promisify(crypto.randomBytes);
+
+crypto.randomBytes(32, (err,num) => {
+  console.log(num)
+});
+
+randomBytes(32)
+  .then(num => console.log(num))
+  .catch(err => {
+
+  })
