6310c082963ad9a41826c106b3db74d6e1dccc63
beataj <beata@emc2wave.com>
2022-08-25 18:10:46 -0700

    create sessionId

--- server/security.utils.ts
+++ server/security.utils.ts
@@ -4,14 +4,14 @@ import crypto = require('crypto');
 import  util  = require('util');
 
 
-const randomBytes = util.promisify(crypto.randomBytes);
+export const randomBytes = util.promisify(crypto.randomBytes);
 
-crypto.randomBytes(32, (err,num) => {
-  console.log(num)
-});
+// crypto.randomBytes(32, (err,num) => {
+//   console.log(num)
+// });
 
-randomBytes(32)
-  .then(num => console.log(num))
-  .catch(err => {
+// randomBytes(32)
+//   .then(num => console.log(num))
+//   .catch(err => {
 
-  })
+//   })
